3f03883d85b8b731df882d21ff72eb57f368d538
Mage-Knight <andriyshabo@gmail.com>
2022-04-22 21:54:09 +0300

    Fix considering the same element twice as pair in count_num_pairs

--- Task4.py
+++ Task4.py
@@ -6,14 +6,11 @@ target_num = 5
 def count_num_pairs(arr, target_num):
     counter = 0
     arr_length = len(arr)
-    if arr_length >= 2:
-        for i in range(arr_length):
-            for j in arr[i:]:
-                if arr[i] + j == target_num:
-                    counter += 1
-        return counter
-    else:
-        return 0
+    for i in range(arr_length-1):
+        for j in arr[i+1:]:
+            if arr[i] + j == target_num:
+                counter += 1
+    return counter
 
 # Output result.
 print(count_num_pairs(arr, target_num))
